ac7f13b244affdb0927f778f29a589b32f926c02
leonardo.viana <leonardo.viana@in8.com.br>
2024-08-11 01:05:55 -0300

    Feat: Improve crawler with xvfb and moving it to controller

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,10 @@ WORKDIR /code
 COPY ./requirements.txt /code/requirements.txt
 
 # 
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
-RUN playwright install --with-deps
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt \
+    && playwright install --with-deps \
+    && apt-get update -y \
+    && apt-get -y install xvfb
 # 
 COPY . /code
 
--- app/controllers/stock_controller.py
+++ app/controllers/stock_controller.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 from typing import List
 from requests_cache.session import CachedSession
 from sqlalchemy.orm import Session
-from app.controllers.crawler import get_web_data
 from app.models.stock_model import (
     Competitor,
     MarketCap,
@@ -21,6 +20,10 @@ from app.schemas.stock_schema import (
 )
 from shared.exceptions import NotFound, PreconditionFailedAmount, PreconditionFailedName
 import logging
+from playwright.sync_api import sync_playwright
+from functools import cache
+from xvfbwrapper import Xvfb
+import random
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -130,7 +133,7 @@ def get_new_stock(company_code: str) -> StockModelResponseSchema:
         StockModelResponseSchema: "Get" route stock model populated
     """
     polygon_data = get_stock_from_polygon(company_code)
-    web_data = get_web_data(company_code)
+    web_data = get_market_watch(company_code)
     return format_scrap_stock_response(polygon_data, web_data)
 
 
@@ -291,3 +294,84 @@ def get_stock_from_polygon(company_code: str) -> PolygonStockData:
         raise NotFound(company_code)
 
     return PolygonStockData(**response.json())
+
+
+@cache
+def get_market_watch(company_code: str) -> MarketWatchData:
+    """Scraps MarketWatch website and gets data on given stock
+
+    Args:
+        company_code (str): company stock code. E.g.: "aapl" or "AAPL"
+
+    Returns:
+        MarketWatchData: pydantic schema with marketwatch website data. If process fails, return empty version of schema
+    """
+    vdisplay = Xvfb()
+    vdisplay.start()
+    try:
+        logger.debug(f"starting crawler for stock {company_code}")
+        user_agents = [
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        ]
+        playwright = sync_playwright().start()
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(user_agent=random.choice(user_agents))
+        context.add_init_script(
+            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+        )
+        page = context.new_page()
+        page.goto(
+            "https://www.google.com.br/", timeout=60000, wait_until="domcontentloaded"
+        )
+        page.goto(
+            f"https://www.marketwatch.com/investing/stock/{company_code}",
+            timeout=60000,
+            wait_until="domcontentloaded",
+        )
+
+        page.locator(".company__name").wait_for(timeout=10000, state="visible")
+        company_name = page.locator(".company__name").first.inner_text()
+        performance_data = page.locator(
+            ".content__item.value.ignore-color"
+        ).all_inner_texts()
+        competitorsRows = page.locator(
+            ".Competitors > table > tbody > tr"
+        ).all_inner_texts()
+        competitors = []
+        for competitorRow in competitorsRows:
+            competitorData = competitorRow.split("\t")
+            competitors.append(
+                CompetitorSchema(
+                    name=competitorData[0],
+                    market_cap=MarketCapSchema(
+                        currency=competitorData[2],
+                        value=float(competitorData[1].strip("%")),
+                    ),
+                )
+            )
+        stock_data = MarketWatchData(
+            company_name=company_name,
+            performance_data=PerformanceDataSchema(
+                five_days=float(performance_data[0].strip("%")),
+                one_month=float(performance_data[1].strip("%")),
+                three_months=float(performance_data[2].strip("%")),
+                year_to_date=float(performance_data[3].strip("%")),
+                one_year=float(performance_data[4].strip("%")),
+            ),
+            competitors=competitors,
+        )
+        browser.close()
+        playwright.stop()
+        vdisplay.stop()
+    except Exception as error:
+        logger.error(error)
+        browser.close()
+        playwright.stop()
+        vdisplay.stop()
+        raise NotFound(company_code)
+
+    return stock_data
